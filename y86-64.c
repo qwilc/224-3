83f52a795ef426822a29495ea9c5e45e72aa9231
Quincy Wilcox <76232629+qwilc@users.noreply.github.com>
2021-10-30 22:31:06 +0000

    Get nop, halt, irmovq, rrmovq working

---
+++ y86-64.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utils.h"
+
+const int MAX_MEM_SIZE  = (1 << 13);
+
+void fetchStage(int *icode, int *ifun, int *rA, int *rB, wordType *valC, wordType *valP) {
+	pc = getPC();
+	byteType byte = getByteFromMemory(pc);
+	*ifun = byte&0xf;
+	*icode = byte >> 4;
+
+	if(*icode == NOP || *icode == HALT) {
+		*valP = pc + 1;
+	}
+	else if (*icode == IRMOVQ) {
+		byte = getByteFromMemory(pc + 1);
+		*rA = byte >> 4;
+		*rB = byte&0xf;
+		*valC = getWordFromMemory(pc+2);
+		*valP = pc + 10;
+	}
+	else if(*icode == RRMOVQ || *icode == OPQ) {
+		byte = getByteFromMemory(pc + 1);
+		*rA = byte >> 4;
+		*rB = byte&0xf;
+		*valP = pc + 2;
+	}
+}
+
+void decodeStage(int icode, int rA, int rB, wordType *valA, wordType *valB) {
+	if(icode == RRMOVQ) {
+		*valA = getRegister(rA);
+	}
+	else if(icode == OPQ) {
+		*valA = getRegister(rA);
+		*valB = getRegister(rB);
+	}
+}
+
+void executeStage(int icode, int ifun, wordType valA, wordType valB, wordType valC, wordType *valE, bool *Cnd) {
+	if(icode == IRMOVQ) {
+		*valE = valC;
+	}
+	else if(icode == RRMOVQ) {
+		*valE = valA;
+	}
+	else if(icode == OPQ) {
+		if(ifun == ADD) {
+			*valE = valA + valB;
+		} 
+		else if(ifun == SUB) {
+
+		} 
+		else if(ifun == AND) {
+
+		} 
+		else if(ifun == XOR) {
+
+		}
+
+		zeroFlag = *valE == 0;
+		signFlag = *valE < 0;
+		overflowFlag = ((valA<0)==(valB<0) && (*valE<0)!=(valA<0));
+
+	}
+  
+}
+
+void memoryStage(int icode, wordType valA, wordType valP, wordType valE, wordType *valM) {
+
+}
+
+void writebackStage(int icode, int rA, int rB, wordType valE, wordType valM) {
+	if(icode == IRMOVQ || icode == RRMOVQ || icode == OPQ) {
+		setRegister(rB, valE);
+	}
+}
+
+void pcUpdateStage(int icode, wordType valC, wordType valP, bool Cnd, wordType valM) {
+  if(icode == NOP || icode == IRMOVQ || icode == RRMOVQ || icode == OPQ) {
+		setPC(valP);
+	}
+	else if(icode == HALT) {
+		setStatus(STAT_HLT);
+		setPC(valP);
+	}
+}
+
+void stepMachine(int stepMode) {
+  /* FETCH STAGE */
+  int icode = 0, ifun = 0;
+  int rA = 0, rB = 0;
+  wordType valC = 0;
+  wordType valP = 0;
+ 
+  /* DECODE STAGE */
+  wordType valA = 0;
+  wordType valB = 0;
+
+  /* EXECUTE STAGE */
+  wordType valE = 0;
+  bool Cnd = 0;
+
+  /* MEMORY STAGE */
+  wordType valM = 0;
+
+  fetchStage(&icode, &ifun, &rA, &rB, &valC, &valP);
+  applyStageStepMode(stepMode, "Fetch", icode, ifun, rA, rB, valC, valP, valA, valB, valE, Cnd, valM);
+
+  decodeStage(icode, rA, rB, &valA, &valB);
+  applyStageStepMode(stepMode, "Decode", icode, ifun, rA, rB, valC, valP, valA, valB, valE, Cnd, valM);
+  
+  executeStage(icode, ifun, valA, valB, valC, &valE, &Cnd);
+  applyStageStepMode(stepMode, "Execute", icode, ifun, rA, rB, valC, valP, valA, valB, valE, Cnd, valM);
+  
+  memoryStage(icode, valA, valP, valE, &valM);
+  applyStageStepMode(stepMode, "Memory", icode, ifun, rA, rB, valC, valP, valA, valB, valE, Cnd, valM);
+  
+  writebackStage(icode, rA, rB, valE, valM);
+  applyStageStepMode(stepMode, "Writeback", icode, ifun, rA, rB, valC, valP, valA, valB, valE, Cnd, valM);
+  
+  pcUpdateStage(icode, valC, valP, Cnd, valM);
+  applyStageStepMode(stepMode, "PC update", icode, ifun, rA, rB, valC, valP, valA, valB, valE, Cnd, valM);
+
+  incrementCycleCounter();
+}
+
+/** 
+ * main
+ * */
+int main(int argc, char **argv) {
+  int stepMode = 0;
+  FILE *input = parseCommandLine(argc, argv, &stepMode);
+
+  initializeMemory(MAX_MEM_SIZE);
+  initializeRegisters();
+  loadMemory(input);
+
+  applyStepMode(stepMode);
+  while (getStatus() != STAT_HLT) {
+    stepMachine(stepMode);
+    applyStepMode(stepMode);
+#ifdef DEBUG
+    printMachineState();
+    printf("\n");
+#endif
+  }
+  printMachineState();
+  return 0;
+}
